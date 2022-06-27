6e8b904dc50c768650e3f351009e74c040ae3d6a
Rizki Karianata <natarizky884@gmail.com>
2022-06-27 23:25:44 +0700

    chore: update name app

--- src/js/app.js
+++ src/js/app.js
@@ -19,7 +19,7 @@ var app = new Framework7({
   root: '#app', // App root element
   component: App, // App main component
   id: 'io.framework7.myapp', // App bundle ID
-  name: 'MyBatik', // App name
+  name: 'Bainart App', // App name
   theme: 'auto', // Automatic theme detection
 
 
